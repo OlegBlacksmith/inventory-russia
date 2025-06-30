f51d4b8574f292457915577aef5a90e846add75b
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 20:45:05 +0400

    add: check mobile

--- src/app/pages/home-page/sections/career-section/career-section.component.ts
+++ src/app/pages/home-page/sections/career-section/career-section.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 import { CareerButtonComponent } from './components/career-button/career-button.component';
+import { ScreenService } from '../../../../services/screen.service';
+import { map } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-career',
   imports: [
     PageHeaderComponent,
-    CareerButtonComponent
+    CareerButtonComponent,
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './career-section.component.html',
-  styleUrl: './career-section.component.scss'
+  styleUrl: './career-section.component.css'
 })
 export class CareerSectionComponent {
+  private screenService = inject(ScreenService);
 
+  isMobile$ = this.screenService.isMobile$;
+  isNotMobile$ = this.screenService.isNotMobile$;
+
+  readonly showIsland$ = this.screenService.isMobile$.pipe(
+    map(isMobile => !isMobile)
+  );
 }
