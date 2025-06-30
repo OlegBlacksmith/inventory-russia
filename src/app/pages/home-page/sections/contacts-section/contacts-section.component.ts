ba9a65956d76bb17a9b240281d0ce609f66f64f2
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 20:53:39 +0400

    add: check mobile

--- src/app/pages/home-page/sections/contacts-section/contacts-section.component.ts
+++ src/app/pages/home-page/sections/contacts-section/contacts-section.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 import { OrderButtonComponent } from '../../../../shared/components/buttons/order-button/order-button.component';
+import { ScreenService } from '../../../../services/screen.service';
+import { map } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
   imports: [
     PageHeaderComponent,
-    OrderButtonComponent
+    OrderButtonComponent,
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './contacts-section.component.html',
-  styleUrl: './contacts-section.component.scss'
+  styleUrl: './contacts-section.component.css'
 })
 export class ContactsSectionComponent {
+  private screenService = inject(ScreenService);
 
+  isMobile$ = this.screenService.isMobile$;
+  isNotMobile$ = this.screenService.isNotMobile$;
+
+  readonly showIsland$ = this.screenService.isMobile$.pipe(
+    map(isMobile => !isMobile)
+  );
 }
