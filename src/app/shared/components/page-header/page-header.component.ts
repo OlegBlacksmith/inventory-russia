6d289d261d258b64219715e3221388fb6dd7f776
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 21:08:55 +0400

    fix: scss to css

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'app-page-header',
   imports: [],
   templateUrl: './page-header.component.html',
-  styleUrl: './page-header.component.scss'
+  styleUrl: './page-header.component.css'
 })
 export class PageHeaderComponent {
   @Input() headerText?: string;
