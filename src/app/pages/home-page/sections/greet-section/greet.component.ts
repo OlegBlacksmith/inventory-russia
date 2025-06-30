e0a551e4bd92e14aef00799c48b605ce5a3a9278
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 20:54:49 +0400

    add: check mobile

--- src/app/pages/home-page/sections/greet-section/greet.component.ts
+++ src/app/pages/home-page/sections/greet-section/greet.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { OrderButtonComponent } from '../../../../shared/components/buttons/order-button/order-button.component';
+import { ScreenService } from '../../../../services/screen.service';
+import { map } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-greet',
   imports: [
-    OrderButtonComponent
+    OrderButtonComponent,
+    // NgIf,
+    // AsyncPipe
   ],
   templateUrl: './greet-section.component.html',
-  styleUrl: './greet-section.component.scss'
+  styleUrl: './greet-section.component.css'
 })
 export class GreetSectionComponent {
+  private screenService = inject(ScreenService);
+
+  isMobile$ = this.screenService.isMobile$;
+  isNotMobile$ = this.screenService.isNotMobile$;
+
+  readonly showNavMenu$ = this.screenService.isMobile$.pipe(
+    map(isMobile => !isMobile)
+  );
 }
