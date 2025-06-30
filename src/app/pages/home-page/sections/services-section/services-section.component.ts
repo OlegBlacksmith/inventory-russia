e31b9953db3199c1563169dae1b94484e71ae1cc
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 20:56:05 +0400

    add: check mobile

--- src/app/pages/home-page/sections/services-section/services-section.component.ts
+++ src/app/pages/home-page/sections/services-section/services-section.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 import { ServiceCardComponent } from './components/service-card/service-card.component';
+import { ScreenService } from '../../../../services/screen.service';
+import { map } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-services',
   imports: [
     PageHeaderComponent,
-    ServiceCardComponent
+    ServiceCardComponent,
+    AsyncPipe,
+    NgIf
   ],
   templateUrl: './services-section.component.html',
-  styleUrl: './services-section.component.scss'
+  styleUrl: './services-section.component.css'
 })
 export class ServicesSectionComponent {
+  private screenService = inject(ScreenService);
 
+  isMobile$ = this.screenService.isMobile$;
+  isNotMobile$ = this.screenService.isNotMobile$;
+
+  readonly showIsland$ = this.screenService.isMobile$.pipe(
+    map(isMobile => !isMobile)
+  );
 }
