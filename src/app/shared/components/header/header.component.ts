15ed3728957089d0a3471015d02e6e27d58235c7
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 21:08:11 +0400

    add: check mobile and mobile menu methods

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { map, Observable } from 'rxjs';
+import { ScreenService } from '../../../services/screen.service';
+import { MobileMenuComponent } from "../mobile-menu/mobile-menu.component";
+import { RouterLink } from '@angular/router';
+import { MobileMenuService } from '../../../services/mobile-menu.service';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NgIf,
+    AsyncPipe,
+    MobileMenuComponent,
+    RouterLink
+],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  private menuService = inject(MobileMenuService);
+  private screenService = inject(ScreenService);
 
+  toggleMenu() {
+    this.menuService.toggle();
+  }
+
+  closeMenu = () => {
+    this.menuService.close();
+  };
+
+  isMobile$ = this.screenService.isMobile$;
+  isNotMobile$ = this.screenService.isNotMobile$;
+  isMenuOpen$ = this.menuService.isOpen$;
+
+  readonly showNavMenu$ = this.screenService.isMobile$.pipe(
+    map(isMobile => !isMobile)
+  );
 }
