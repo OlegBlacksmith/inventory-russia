d5e8e677134e1df42f26fe4d6e08c9934bf1ea57
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-06-30 20:48:14 +0400

    add: new carousel component and check mobile

--- src/app/pages/home-page/sections/client-section/clients-section.component.ts
+++ src/app/pages/home-page/sections/client-section/clients-section.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { ScreenService } from '../../../../services/screen.service';
+import { map } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { GratitudeCarouselComponent } from './components/gratitude-carousel/gratitude-carousel.component';
 
 @Component({
   selector: 'app-clients',
   imports: [
     PageHeaderComponent,
-    CarouselComponent
+    CarouselComponent,
+    GratitudeCarouselComponent,
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './clients-section.component.html',
-  styleUrl: './clients-section.component.scss'
+  styleUrl: './clients-section.component.css'
 })
 export class ClientsSectionComponent {
+  private screenService = inject(ScreenService);
 
+  isMobile$ = this.screenService.isMobile$;
+  isNotMobile$ = this.screenService.isNotMobile$;
+
+  readonly showIsland$ = this.screenService.isMobile$.pipe(
+    map(isMobile => !isMobile)
+  );
 }
