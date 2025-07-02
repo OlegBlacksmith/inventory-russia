93910a56c9214128dafadff9828fff49ff67aa25
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-07-02 20:01:06 +0400

    fix: new rule for tablet

--- src/app/services/screen.service.ts
+++ src/app/services/screen.service.ts
@@ -10,14 +10,14 @@ export class ScreenService {
   private breakpointObserver = inject(BreakpointObserver);
 
   public readonly isMobile$: Observable<boolean> = this.breakpointObserver
-    .observe(['(min-width: 375px) and (max-width: 770px)'])
+    .observe(['(min-width: 375px) and (max-width: 767px)'])
     .pipe(
       map(result => result.matches),
       shareReplay({ bufferSize: 1, refCount: true })
     );
 
-  public readonly isNotMobile$: Observable<boolean> = this.breakpointObserver
-    .observe(['(min-width: 771px)'])
+  public readonly isTablet$: Observable<boolean> = this.breakpointObserver
+    .observe(['(min-width: 768px) and (max-width: 1024px)'])
     .pipe(
       map(result => result.matches),
       shareReplay({ bufferSize: 1, refCount: true })
