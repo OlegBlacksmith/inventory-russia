c542a1f866b82d68b68ad28d8322496852c9d4d1
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-07 14:06:28 +0400

    add: new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,29 @@ import { AdditionalPageComponent } from './pages/additional-page/additional-page
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 import { PersonalDataComponent } from './pages/personal-data/personal-data.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { PublicLayoutComponent } from './pages/layouts/public-layout/public-layout.component';
+import { AdminLayoutComponent } from './pages/layouts/admin-layout/admin-layout.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent  },
-  { path: 'additional/:type', component: AdditionalPageComponent  },
-  { path: 'privacy-policy', component: PrivacyPolicyComponent  },
-  { path: 'personal-data', component: PersonalDataComponent  },
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      { path: '', component: HomePageComponent },
+      { path: 'additional/:type', component: AdditionalPageComponent },
+      { path: 'privacy-policy', component: PrivacyPolicyComponent },
+      { path: 'personal-data', component: PersonalDataComponent },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    canActivate: [AuthGuard], // 👈 ЗАЩИТА
+    children: [{ path: '', component: AdminPageComponent }],
+  },
+  { path: 'login', component: LoginPageComponent },
   { path: '**', component: NotFoundPageComponent },
 ];
