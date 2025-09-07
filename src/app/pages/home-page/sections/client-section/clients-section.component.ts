65dab0dec9f69d0ce9877d052dd22625d31fdea6
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-07 14:12:58 +0400

    add: partners and gratitude in carousels

--- src/app/pages/home-page/sections/client-section/clients-section.component.ts
+++ src/app/pages/home-page/sections/client-section/clients-section.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { ScreenService } from '../../../../services/screen.service';
 import { combineLatest, map } from 'rxjs';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { GratitudeCarouselComponent } from './components/gratitude-carousel/gratitude-carousel.component';
+import { PartnersService } from '../../../../services/partners.service';
+import { GratitudesService } from '../../../../services/gratitudes.service';
 
 @Component({
   selector: 'app-clients',
@@ -18,8 +20,23 @@ import { GratitudeCarouselComponent } from './components/gratitude-carousel/grat
   templateUrl: './clients-section.component.html',
   styleUrl: './clients-section.component.css'
 })
-export class ClientsSectionComponent {
+export class ClientsSectionComponent implements OnInit{
+  partners: string[] = [];
+  gratitude: string[] = [];
   private screenService = inject(ScreenService);
+
+  constructor(private partService: PartnersService, private gratService: GratitudesService){}
+
+  async ngOnInit() {
+    try {
+      this.partners = await this.partService.getPartnerPicsList();
+      this.gratitude = await this.gratService.getGratitudesList();
+    } catch (err) {
+      console.error(err);
+      this.partners = [];
+      this.gratitude = [];
+    }
+  }
 
   isMobile$ = this.screenService.isMobile$;
   isTablet$ = this.screenService.isTablet$;
