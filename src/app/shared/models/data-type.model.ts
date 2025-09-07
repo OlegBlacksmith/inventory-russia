ff00da28232d243c43aed8e58e7a385b7ed78e9d
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-07 14:32:07 +0400

    add: data type model

---
+++ src/app/shared/models/data-type.model.ts
@@ -0,0 +1,13 @@
+export const DATA_TYPE: Record<string, string> = {
+  companyInfo: 'Информация о компании',
+  details: 'Реквизиты',
+  uploadPartner: 'Добавлен партнер',
+  deletePartner: 'Партнер удален',
+  updateCompanyInfo: 'Информация о компании обновлена',
+  updateDetails: 'Реквизиты обновлены',
+  uploadGratitude: 'Добавлена благодарность',
+  deleteGratitude: 'Удалена благодарность',
+  vacancy: 'Вакансии',
+  deleteLogo: 'Логотип удален',
+  uploadLogos: 'Логотип добавлен'
+}
