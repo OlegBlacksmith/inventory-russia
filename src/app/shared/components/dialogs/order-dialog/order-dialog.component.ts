13622dfa59d6214370e02011cfcd348f2dc7fe66
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-07 14:27:56 +0400

    add: notification

--- src/app/shared/components/dialogs/order-dialog/order-dialog.component.ts
+++ src/app/shared/components/dialogs/order-dialog/order-dialog.component.ts
@@ -2,27 +2,22 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { OrderModalService } from '../../../../services/order-modal.service';
 import { CommonModule, NgIf } from '@angular/common';
 import { Subscription } from 'rxjs';
-import {
-  trigger,
-  transition,
-  style,
-  animate,
-} from '@angular/animations';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MailService } from '../../../../services/mail.service';
-
+import { NotificationService } from '../../../../services/notification.service';
 
 @Component({
   standalone: true,
   selector: 'app-order-dialog',
-  imports: [
-    RouterLink,
-    ReactiveFormsModule,
-    FormsModule,
-    CommonModule,
-    NgIf
-  ],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule, CommonModule, NgIf],
   templateUrl: './order-dialog.component.html',
   styleUrl: './order-dialog.component.css',
   animations: [
@@ -31,11 +26,9 @@ import { MailService } from '../../../../services/mail.service';
         style({ opacity: 0 }),
         animate('200ms ease-in', style({ opacity: 1 })),
       ]),
-      transition(':leave', [
-        animate('200ms ease-out', style({ opacity: 0 })),
-      ]),
-    ])
-  ]
+      transition(':leave', [animate('200ms ease-out', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class OrderDialogComponent implements OnInit, OnDestroy {
   visible = false;
@@ -45,7 +38,8 @@ export class OrderDialogComponent implements OnInit, OnDestroy {
   constructor(
     private dialogService: OrderModalService,
     private fb: FormBuilder,
-    private mailService: MailService
+    private mailService: MailService,
+    private notificationService: NotificationService
   ) {
     this.form = this.fb.group({
       name: ['', Validators.required],
@@ -74,24 +68,49 @@ export class OrderDialogComponent implements OnInit, OnDestroy {
 
   onSubmit() {
     if (this.form.invalid) {
-      alert('Пожалуйста, заполните все поля и подтвердите согласие.');
+      this.notificationService.show({
+        message: 'Пожалуйста, заполните все поля',
+        text: 'и подтвердите согласие на обработку данных!',
+        type: 'warn',
+      });
+      console.log('в рот ебал, вызвалось!')
       return;
     }
 
     const { name, phone, email, callTime } = this.form.value;
 
+    const callTimeMap: Record<string, string> = {
+      soon: 'в ближайшее время',
+      duringTheDay: 'в течение дня',
+      onlyPost: 'предпочитает почту',
+    };
+    const callTimeLabel = callTimeMap[callTime] || callTime;
+
     const formData = new FormData();
     formData.append('name', name);
     formData.append('email', email);
-    formData.append('message', `Телефон: ${phone}\nКогда перезвонить: ${callTime}`);
+    formData.append(
+      'message',
+      `Телефон: ${phone}\nПочта: ${email}\nКогда перезвонить: ${callTimeLabel}`
+    );
 
     this.mailService.sendEmailReg(formData).subscribe({
       next: () => {
-        alert('Заявка успешно отправлена!');
+        this.notificationService.show({
+          message: 'Заявка отправлена!',
+          text: 'Ждите звонка',
+          type: 'success',
+        });
         this.form.reset();
         this.onClose();
       },
-      error: () => alert('Ошибка при отправке заявки. Попробуйте позже.'),
+      error: () => {
+        this.notificationService.show({
+          message: 'Ошибка отправки!',
+          text: 'Попробуйте снова',
+          type: 'error',
+        });
+      },
     });
   }
 }
