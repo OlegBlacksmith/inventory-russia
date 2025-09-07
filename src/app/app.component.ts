5a75ef0297b07a7c8e1419e8c817ee87ac5cf432
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-07 14:05:25 +0400

    remove: components to layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { Meta, Title } from '@angular/platform-browser';
-import { OrderDialogComponent } from './shared/components/dialogs/order-dialog/order-dialog.component';
-import { CareerDialogComponent } from './shared/components/dialogs/career-dialog/career-dialog.component';
-import { CoockieBubbleComponent } from './shared/components/coockie-bubble/coockie-bubble.component';
-import { FabMenuComponent } from "./shared/components/fab-menu/fab-menu.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    HeaderComponent,
-    FooterComponent,
-    OrderDialogComponent,
-    CareerDialogComponent,
-    CoockieBubbleComponent,
-    FabMenuComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
