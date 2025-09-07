b40b7295219135908c06ed7cd20c4670955baf8a
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-07 14:32:47 +0400

    add: field labels

---
+++ src/app/shared/models/field-labels.model.ts
@@ -0,0 +1,14 @@
+export const FIELD_LABELS: Record<string, string> = {
+  title: 'Название',
+  description: 'Описание',
+  email: 'Электронная почта',
+  phone: 'Телефон',
+  address: 'Адрес',
+  ogrn: 'ОГРН',
+  inn: 'ИНН',
+  kpp: 'КПП',
+  rs: 'Р/С',
+  bik: 'БИК',
+  bankName: 'Название банка',
+  ks: 'К/С',
+};
