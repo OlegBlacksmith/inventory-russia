5dd4aed205893c78708d1908e20bfae9016f0d39
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-12 15:41:43 +0400

    add: scroll to

--- src/app/pages/personal-data/personal-data.component.ts
+++ src/app/pages/personal-data/personal-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackButtonComponent } from '../../shared/components/buttons/back-button/back-button.component';
 import { personalDataContent } from '../../shared/data/personal-data-content';
 import { NgFor } from '@angular/common';
@@ -14,6 +14,10 @@ import { PageHeaderComponent } from '../../shared/components/page-header/page-he
   templateUrl: './personal-data.component.html',
   styleUrl: './personal-data.component.css'
 })
-export class PersonalDataComponent {
+export class PersonalDataComponent implements OnInit{
   contents = personalDataContent;
+
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 }
