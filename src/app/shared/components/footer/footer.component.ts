d884ce21c380ac2b7a2e8b54c9c5f66de47ceee7
Oleg Blacksmith <OKuznetsov21041992@outlook.com>
2025-09-12 15:43:01 +0400

    add: router link

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { LogoInfo } from '../../models/logo-info.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
